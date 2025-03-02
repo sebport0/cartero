550e8db42f3296d9a5301aa19ceaa8ee30e8fec8
sebport0 <8371137+sebport0@users.noreply.github.com>
2025-03-02 19:05:16 -0300

    add app

--- cmd/cartero/app/app.go
+++ cmd/cartero/app/app.go
@@ -1,1 +1,32 @@
 package app
+
+import (
+	"github.com/sebport0/cartero/cmd/cartero/app/backend"
+	"github.com/sebport0/cartero/cmd/cartero/app/tui"
+	"github.com/sebport0/cartero/internal/collection"
+	"github.com/sebport0/cartero/internal/request"
+)
+
+type App struct {
+	tui *tui.TUI
+	be  *backend.Backend
+}
+
+func NewApp() *App {
+	t := tui.NewTUI()
+	be := backend.NewBackend()
+	return &App{tui: t, be: be}
+}
+
+func (a *App) CreateEmptyCollection(name string) {
+	c := collection.NewCollection(name, map[string]*request.Request{})
+	a.be.AddCollection(c)
+}
+
+func (a *App) AddRequestToCollection(collection string, r *request.Request) {
+	a.be.AddRequest(collection, r)
+}
+
+func (a *App) DeleteReqquest(collection, request string) {
+	a.be.DeleteRequest(collection, request)
+}
